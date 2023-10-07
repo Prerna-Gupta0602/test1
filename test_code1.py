17479164f0714d45b6d49573df80761f34b150a4
Prerna-Gupta0602 <prerna.gupta0602@gmail.com>
2023-10-07 13:34:49 +0530

    first version of a pandas code

---
+++ test_code1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 13:25:50 2023
+
+@author: engga
+"""
+
+
+import pandas as pd
+import os
+
+df = pd.DataFrame()
+print(df)
+os.chdir(r'C:\Users\engga\OneDrive\Desktop\git_repo\test1')
+print(os.getcwd())
