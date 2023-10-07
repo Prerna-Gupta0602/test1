71866c9cd35b89d09c2c8940dd0020083b42264d
Prerna-Gupta0602 <prerna.gupta0602@gmail.com>
2023-10-07 13:49:09 +0530

    second version of the pandas code

--- test_code1.py
+++ test_code1.py
@@ -7,9 +7,7 @@ Created on Sat Oct  7 13:25:50 2023
 
 
 import pandas as pd
-import os
 
-df = pd.DataFrame()
+a = {'x':[1,2,3],'y':[4,5,6]}
+df = pd.DataFrame(a)
 print(df)
-os.chdir(r'C:\Users\engga\OneDrive\Desktop\git_repo\test1')
-print(os.getcwd())
